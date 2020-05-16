ba9cb0c0212d19fbbd9e283468bee8440f86f0b9
DayanOA <42161456+DayanOA@users.noreply.github.com>
2020-05-15 20:39:15 -0500

    Add files via upload

---
+++ Patron Bridge/PatronBridge/src/patronbridge/BMW.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patronbridge;
+
+/**
+ *
+ * @author Tonny
+ */
+public class BMW extends Car {
+
+    public BMW(Workshop workShop1, Workshop workShop2, Workshop workShop3, Workshop workShop4) {
+        super(workShop1, workShop2, workShop3, workShop4);
+    }
+
+    @Override
+    public void production_house() {
+        System.out.print("BMW Car ");
+        workShop1.work();
+        workShop2.work();
+        workShop3.work();
+        workShop4.work();
+    }
+
+}
---
+++ Patron Bridge/PatronBridge/src/patronbridge/Tunning.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patronbridge;
+
+/**
+ *
+ * @author Tonny
+ */
+public class Tunning implements Workshop {
+
+    @Override
+    public void work() {
+        System.out.print("Tunning, ");
+    }
+
+}
